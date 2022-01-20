1f1181677185d9929c88371daee2c37f95ed6b9d
Niclas Hoyer <info@niclashoyer.de>
2022-01-20 20:23:12 +0100

    rustfmt

--- examples/in_out.rs
+++ examples/in_out.rs
@@ -6,47 +6,47 @@ use std::io::{BufReader, BufWriter};
 // read in a vcd file and write it out again using bit banging
 
 fn main() -> Result<(), std::io::Error> {
-	// construct a reader
-	let f = BufReader::new(File::open("examples/data/test.vcd")?);
-	let mut reader = VcdReader::new(f).unwrap();
-	// get the input pin from the reader
-	let in_pin = reader.get_pin(&["libsigrok", "data"]).unwrap();
+    // construct a reader
+    let f = BufReader::new(File::open("examples/data/test.vcd")?);
+    let mut reader = VcdReader::new(f).unwrap();
+    // get the input pin from the reader
+    let in_pin = reader.get_pin(&["libsigrok", "data"]).unwrap();
 
-	// construct a writer builder
-	let f2 = BufWriter::new(File::create("examples/data/test2.vcd")?);
-	let mut writer = VcdWriterBuilder::new(f2).unwrap();
-	// add output pin to writer
-	let mut out_pin = writer.add_push_pull_pin("data")?;
-	// build the writer
-	let mut writer = writer.build().unwrap();
+    // construct a writer builder
+    let f2 = BufWriter::new(File::create("examples/data/test2.vcd")?);
+    let mut writer = VcdWriterBuilder::new(f2).unwrap();
+    // add output pin to writer
+    let mut out_pin = writer.add_push_pull_pin("data")?;
+    // build the writer
+    let mut writer = writer.build().unwrap();
 
-	// closure used to copy the input pin from the reader
-	// to the output pin of the writer
-	let mut copy_pins = || {
-		if in_pin.is_high().unwrap() {
-			out_pin.set_high().unwrap();
-		} else {
-			out_pin.set_low().unwrap();
-		}
-	};
+    // closure used to copy the input pin from the reader
+    // to the output pin of the writer
+    let mut copy_pins = || {
+        if in_pin.is_high().unwrap() {
+            out_pin.set_high().unwrap();
+        } else {
+            out_pin.set_low().unwrap();
+        }
+    };
 
-	// get first timestamp from vcd and pass it to the writer
-	writer.timestamp(reader.next().unwrap())?;
+    // get first timestamp from vcd and pass it to the writer
+    writer.timestamp(reader.next().unwrap())?;
 
-	// Iterate over all change events in input file.
-	// The iterator will yield each timestamp *before* the value
-	// changes are applied, so we first need to get a timestamp,
-	// save it to `last_t` and start from there.
-	for t in reader {
-		copy_pins();
-		writer.sample()?;
-		writer.timestamp(t)?;
-	}
-	// no timestamp left in file, we need to copy
-	// the final pin values to the output and
-	// push a last sample
-	copy_pins();
-	writer.sample()?;
+    // Iterate over all change events in input file.
+    // The iterator will yield each timestamp *before* the value
+    // changes are applied, so we first need to get a timestamp,
+    // save it to `last_t` and start from there.
+    for t in reader {
+        copy_pins();
+        writer.sample()?;
+        writer.timestamp(t)?;
+    }
+    // no timestamp left in file, we need to copy
+    // the final pin values to the output and
+    // push a last sample
+    copy_pins();
+    writer.sample()?;
 
-	Ok(())
+    Ok(())
 }
--- src/reader.rs
+++ src/reader.rs
@@ -8,84 +8,84 @@ use std::sync::Arc;
 
 pub struct VcdReader<R>
 where
-	R: std::io::Read,
+    R: std::io::Read,
 {
-	parser: vcd::Parser<R>,
-	scale: Generic<u64>,
-	header: vcd::Header,
-	pins: FnvHashMap<vcd::IdCode, Arc<AtomicPinState>>,
+    parser: vcd::Parser<R>,
+    scale: Generic<u64>,
+    header: vcd::Header,
+    pins: FnvHashMap<vcd::IdCode, Arc<AtomicPinState>>,
 }
 
 impl<R> VcdReader<R>
 where
-	R: std::io::Read,
+    R: std::io::Read,
 {
-	pub fn new(read: R) -> IOResult<Self> {
-		let mut parser = vcd::Parser::new(read);
-		let header = parser.parse_header()?;
-		let scale = Self::timescale_to_duration(&header).unwrap();
-		Ok(Self {
-			parser,
-			header,
-			scale,
-			pins: FnvHashMap::default(),
-		})
-	}
+    pub fn new(read: R) -> IOResult<Self> {
+        let mut parser = vcd::Parser::new(read);
+        let header = parser.parse_header()?;
+        let scale = Self::timescale_to_duration(&header).unwrap();
+        Ok(Self {
+            parser,
+            header,
+            scale,
+            pins: FnvHashMap::default(),
+        })
+    }
 
-	pub fn scale(&self) -> Generic<u64> {
-		self.scale
-	}
+    pub fn scale(&self) -> Generic<u64> {
+        self.scale
+    }
 
-	fn timescale_to_duration(header: &vcd::Header) -> Option<Generic<u64>> {
-		if let Some((scale, unit)) = header.timescale {
-			let fraction = Fraction::new(1, unit.divisor() as u32);
-			Some(Generic::new(scale as u64, fraction))
-		} else {
-			None
-		}
-	}
+    fn timescale_to_duration(header: &vcd::Header) -> Option<Generic<u64>> {
+        if let Some((scale, unit)) = header.timescale {
+            let fraction = Fraction::new(1, unit.divisor() as u32);
+            Some(Generic::new(scale as u64, fraction))
+        } else {
+            None
+        }
+    }
 
-	pub fn get_pin<S>(&mut self, path: &[S]) -> Option<InputPin>
-	where
-		S: Borrow<str>,
-	{
-		if let Some(v) = self.header.find_var(path) {
-			let state = Arc::new(AtomicPinState::new_with_state(PinState::Floating));
-			let pin = InputPin::new(state.clone());
-			self.pins.insert(v.code, state);
-			Some(pin)
-		} else {
-			None
-		}
-	}
+    pub fn get_pin<S>(&mut self, path: &[S]) -> Option<InputPin>
+    where
+        S: Borrow<str>,
+    {
+        if let Some(v) = self.header.find_var(path) {
+            let state = Arc::new(AtomicPinState::new_with_state(PinState::Floating));
+            let pin = InputPin::new(state.clone());
+            self.pins.insert(v.code, state);
+            Some(pin)
+        } else {
+            None
+        }
+    }
 }
 
 impl<R> Iterator for VcdReader<R>
 where
-	R: std::io::Read,
+    R: std::io::Read,
 {
-	type Item = Generic<u64>;
+    type Item = Generic<u64>;
 
-	fn next(&mut self) -> Option<Self::Item> {
-		use vcd::Command::*;
-		let mut timestamp = None;
-		while let Some(cmd) = self.parser.next() {
-			match cmd {
-				Ok(Timestamp(t)) => {
-					timestamp = Some(Generic::new(
-						self.scale.integer() * t,
-						*self.scale.scaling_factor(),
-					));
-					break;
-				}
-				Ok(ChangeScalar(id, val)) => {
-					if let Some(pin) = self.pins.get_mut(&id) {
-						(*pin).store(val.into(), Ordering::SeqCst);
-					}
-				}
-				_ => {}
-			}
-		}
-		timestamp
-	}
+    fn next(&mut self) -> Option<Self::Item> {
+        use vcd::Command::*;
+        let mut timestamp = None;
+        while let Some(cmd) = self.parser.next() {
+            match cmd {
+                Ok(Timestamp(t)) => {
+                    timestamp = Some(Generic::new(
+                        self.scale.integer() * t,
+                        *self.scale.scaling_factor(),
+                    ));
+                    break;
+                }
+                Ok(ChangeScalar(id, val)) => {
+                    if let Some(pin) = self.pins.get_mut(&id) {
+                        (*pin).store(val.into(), Ordering::SeqCst);
+                    }
+                }
+                _ => {}
+            }
+        }
+        timestamp
+    }
 }
